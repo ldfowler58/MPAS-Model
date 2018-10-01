E3SM-Project/E3SM

commit e314dbc06c883673ab437271f8ca309e3cb0b6c3

Update compiler names in "xlf" build target
For ongoing work on newer systems with the XL compilers, we need to use
the following compilers and MPI wrappers: xlf2003_r, xlc_r, xlc++_r,
mpifort, mpicc, and mpic++.

Makefile
@@ -6,12 +6,12 @@ dummy:
 
 xlf:
 	( $(MAKE) all \
-	"FC_PARALLEL = mpxlf90" \
-	"CC_PARALLEL = mpcc" \
-	"CXX_PARALLEL = mpixlcxx" \
-	"FC_SERIAL = xlf90" \
-	"CC_SERIAL = xlc" \
-	"CXX_SERIAL = xlcxx" \
+	"FC_PARALLEL = mpifort" \
+	"CC_PARALLEL = mpicc" \
+	"CXX_PARALLEL = mpic++" \
+	"FC_SERIAL = xlf2003_r" \
+	"CC_SERIAL = xlc_r" \
+	"CXX_SERIAL = xlc++_r" \
 	"FFLAGS_PROMOTION = -qrealsize=8" \
 	"FFLAGS_OPT = -O3 -qufmt=be" \
 	"CFLAGS_OPT = -O3" \

ldfowler58/MPAS-Model

commit 1746c9d9073efac5838a0dc31c6a3217bc6aef9d

Add PIO2 include path to CPPFLAGS
This fixes a bug introduced in PR #38 that removed the PIO2 include path from CPPFLAGS.  The path is required during the C preprocessor stage when F90 files are not being generated (GEN_F90=false).

Makefile
@@ -402,7 +402,7 @@ LIBS =
 ifneq ($(wildcard $(PIO)/lib), ) # Check for newer PIO version
 ifeq "$(USE_PIO2)" "true"
 	FCINCLUDES = -I$(PIO)/include
-	override CPPFLAGS += -DUSE_PIO2
+	override CPPFLAGS += -DUSE_PIO2 -I$(PIO)/include
 	LIBS = -L$(PIO)/lib -lpiof -lpioc
 ifneq ($(wildcard $(PIO)/lib/libgptl.a), ) # Check for GPTL library for PIO2
 	LIBS += -lgptl
@@ -415,7 +415,7 @@ endif
 else
 ifeq "$(USE_PIO2)" "true"
 	FCINCLUDES = -I$(PIO)/include
-	override CPPFLAGS += -DUSE_PIO2
+	override CPPFLAGS += -DUSE_PIO2 -I$(PIO)/include
 	LIBS = -L$(PIO) -lpiof -lpioc
 ifneq ($(wildcard $(PIO)/libgptl.a), ) # Check for GPTL library for PIO2
 	LIBS += -lgptl