53ff1cb0bb2534dc33584b58264ee6b02ce70394
Laura Fowler <laura@ucar.edu>
2025-04-25 09:42:36 -0600

    * In ./src/core_atmosphere/chemistry:
      -> renamed Registry_gocart2G_tophysics.xml to Registry_gocart2G_forMPASphysics.xml.
      -> renamed mpas_chemistry_gocart2G_tophysics.F90 to mpas_chemistry_gocart2G_forMPASphysics.F90.

--- src/core_atmosphere/chemistry/mpas_chemistry_gocart2G_vars.F90
+++ src/core_atmosphere/chemistry/mpas_chemistry_gocart2G_vars.F90
@@ -30,11 +30,7 @@
 
  use mpas_chemistry_gocart2G_interface,only: atm_gocart2G
  use mpas_chemistry_gocart2G_emissions,only: emis_gocart2G
-<<<<<<< HEAD
  use mpas_chemistry_gocart2G_forMPASphysics,only: chem_gocart2G
-=======
- use mpas_chemistry_gocart2G_tophysics,only: chem_gocart2G
->>>>>>> 3a1a1f59 (* In ./src/core_atmosphere/chemistry, connected the interface between gocart2G and physics.)
 
 
  type(CA2G_bc_GridComp) :: CA2G_bc_params
